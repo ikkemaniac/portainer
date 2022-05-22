15112740b3d0e7aab920a4aab77930a679558f1c
itsconquest <william.conquest@portainer.io>
2022-05-23 11:58:12 +1200

    fix slider updating + add remind me later button

--- app/portainer/components/index.js
+++ app/portainer/components/index.js
@@ -14,7 +14,7 @@ import { ReactExampleAngular } from './ReactExample';
 import { TooltipAngular } from './Tip/Tooltip';
 import { beFeatureIndicatorAngular } from './BEFeatureIndicator';
 import { InformationPanelAngular } from './InformationPanel';
-import { ForcePasswordUpdateHintAngular, PasswordCheckHintAngular } from './PasswordCheckHint';
+import { PasswordCheckHintAngular } from './PasswordCheckHint';
 
 export default angular
   .module('portainer.app.components', [headerModule, boxSelectorModule, widgetModule, sidebarModule, gitFormModule, porAccessManagementModule, formComponentsModule])
@@ -22,6 +22,5 @@ export default angular
   .component('portainerTooltip', TooltipAngular)
   .component('reactExample', ReactExampleAngular)
   .component('beFeatureIndicator', beFeatureIndicatorAngular)
-  .component('forcePasswordUpdateHint', ForcePasswordUpdateHintAngular)
   .component('passwordCheckHint', PasswordCheckHintAngular)
   .component('createAccessToken', CreateAccessTokenAngular).name;
