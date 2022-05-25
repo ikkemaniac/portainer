0b09a44f16170f18d7df2289c9e5132a5f62a2dd
Chaim Lev-Ari <chiptus@gmail.com>
2022-05-25 08:24:19 +0300

    refactor(custom-templates): move variables field to react

--- app/portainer/react/components/index.ts
+++ app/portainer/react/components/index.ts
@@ -1,6 +1,8 @@
 import angular from 'angular';
 
+import { customTemplatesModule } from './custom-templates';
+
 export const componentsModule = angular.module(
   'portainer.app.react.components',
-  []
+  [customTemplatesModule]
 ).name;
